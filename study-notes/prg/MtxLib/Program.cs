9ef5185abdb057ea34cb97fd9cc5601279044469
hartmaj2 <135450351+hartmaj2@users.noreply.github.com>
2025-06-04 13:38:23 +0200

    Create semaphore solution

--- study-notes/prg/MtxLib/Program.cs
+++ study-notes/prg/MtxLib/Program.cs
@@ -1,96 +1,40 @@
-﻿using static System.Console;
-using System.Diagnostics;
-using System.Threading;
-
-var sem = new SemaphoreSlim(5);
-
-int tasks_amount = 20;
-var rand = new Random();
-
-List<long> numbers_to_factorize = [23_567_385_546, 39_567_385_542, 47_567_385_547, 56_567_385_549];
-
-int fin = numbers_to_factorize.Count;
-for (int i = 0; i < tasks_amount - fin; ++i)
+﻿class Program
 {
-    numbers_to_factorize.Add(rand.NextInt64(10_000_000_000, 100_000_000_000));
-}
-
-Write($"{numbers_to_factorize.Count}\n");
-
-static List<long> FactorizeAndPrint(long number, SemaphoreSlim sem)
-{
-    sem.Wait();
-
-    Write($"{Thread.CurrentThread.ManagedThreadId} entered \n");
-
-    List<long> factors = [];
-    long i = 2; // if this is an integer I get an overflow :)
-    while (number != 1)
+    private static Semaphore sem1;
+    private static Semaphore sem2;
+    private static void f1()
     {
-        if (number % i == 0)
-        {
-            factors.Add(i);
-            number /= i;
-            continue;
-        }
-        i++;
+        Console.Write("One");
+        sem1.Release();
+        sem2.WaitOne();
+        Console.Write("Three");
+        sem1.Release();
+        sem2.WaitOne();
+        Console.Write("Five");
     }
 
-    PrintList(factors);
-
-    sem.Release();
-
-    Write($"{Thread.CurrentThread.ManagedThreadId} exited \n");
-    return factors;
-}
-
-static void PrintList<T>(List<T> list)
-{
-    foreach (T elem in list)
+    private static void f2()
     {
-        Write($"{elem}, ");
-    }
-    WriteLine();
-}
-
-static void FactorizeWithThreading(List<long> nums_to_fact, SemaphoreSlim sem)
-{
-    List<Thread> threads = [];
-
-    foreach (var num in nums_to_fact)
-    {
-        var t = new Thread(() => FactorizeAndPrint(num, sem));
-        threads.Add(t);
-        t.Start();
+        Thread.Sleep(1000);
+        sem1.WaitOne();
+        Console.Write("Two");
+        sem2.Release();
+        sem1.WaitOne();
+        Console.Write("Four");
+        sem2.Release();
+        
     }
 
-    foreach (var t in threads)
+    static void Main(string[] args)
     {
-        t.Join();
+        sem1 = new Semaphore(0, 1);
+        sem2 = new Semaphore(0, 1);
+        Thread t1 = new Thread(f1);
+        
+        Thread t2 = new Thread(f2);
+        t1.Start();
+        t2.Start();
+        t1.Join();
+        t2.Join();
     }
-}
-
-static void FactorizeUsingThreadPool(List<long> nums_to_fact, SemaphoreSlim sem)
-{
-    CountdownEvent countdown = new CountdownEvent(nums_to_fact.Count);
-    foreach (var num in nums_to_fact)
-    {
-        ThreadPool.QueueUserWorkItem(state =>
-        {
-            FactorizeAndPrint(num, sem);
-            countdown.Signal();
-        });
-    }
-    countdown.Wait();
-}
-
-var stopwatch = new Stopwatch();
-
-stopwatch.Start();
-
-FactorizeWithThreading(numbers_to_factorize,sem);
-
-stopwatch.Stop();
-Write($"{stopwatch.ElapsedMilliseconds}\n"); // took 12907 milliseconds
-
-delegate void MyCoolFunc();
+ }
